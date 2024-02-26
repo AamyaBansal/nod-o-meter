1683b4f08b17813c29413e20c95f5723a903a68d
Saharsh1005 <saharshbarve3@gmail.com>
2024-02-25 19:44:29 -0600

    feat: using sockets for response logging

--- web/app.py
+++ web/app.py
@@ -5,13 +5,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from vision.face_tracking import track_face_movement
 from question_prompt.generate_question import get_question
 import cv2
+from flask_socketio import SocketIO, emit
+
 # import requests
 # import json
 
 
 app            = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 camera         = cv2.VideoCapture(0)
 # camera = cv2.VideoCapture("D:/Study/UIUC/Projects/head_nod_recognition/src/data/webcam_input.mp4")
+current_question = None
 
 @app.route('/')
 def index():
@@ -19,11 +25,18 @@ def index():
 
 @app.route('/start')
 def start():
-    return Response(track_face_movement(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(track_face_movement(camera,current_question,socketio), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/question')
 def question():
-    return {'question': get_question()}
+    global current_question
+    current_question = get_question()
+    return {'question': current_question}
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+    emit('question', {'question': current_question})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- web/static/script.js
+++ web/static/script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let generateBtn = document.getElementById('generate-btn');
-    let stopBtn     = document.getElementById('stop-btn');
+    let generateBtn   = document.getElementById('generate-btn');
+    let submitBtn     = document.getElementById('submit-btn');
     const videoStream = document.getElementById('video-stream');
     let questionField = document.getElementById('question');
+    let socket        = io.connect('http://' + document.domain + ':' + location.port);
+    let loggedQuestions = []
+    let logTableBody  = document.getElementById('log-table-body');
+
+    let clickCount_play = 0;
+    let clickCount_log = 0;
 
     generateBtn.addEventListener('click', () => {
         fetch('/question')
@@ -11,10 +17,46 @@ document.addEventListener('DOMContentLoaded', () => {
             questionField.textContent = data.question;
             videoStream.src = "/start";
         });
-    });
-    stopBtn.addEventListener('click', () => {
-        questionField.textContent = 'Answer the question by nodding your head..Are you ready?';
-        videoStream.src = src="/static/curious_default.jpg";
+        clickCount_play++;
+        if (clickCount_play === 1) {
+            generateBtn.textContent = "Play Again";
+        }
+        submitBtn.classList.remove('green');
 
     });
+
+    submitBtn.addEventListener('click', () => {
+        questionField.textContent = 'Answer the question by nodding your head..Are you ready?';
+        videoStream.src           = src="/static/curious_default.jpg";
+        submitBtn.classList.add('green');
+        clickCount_log++;
+        if (clickCount_log === 1) {
+            submitBtn.textContent = "Reset";
+        }
+        socket.on('log', function(data) {        
+            // Split the log message into question, gesture, and timestamp
+            let parts     = data.split(' - ');
+            let question  = parts[0].split(': ')[1];
+            let gesture   = parts[1].split(': ')[1];
+            let timestamp = parts[2].split(': ')[1];
+            
+            if (!loggedQuestions.includes(question)){
+    
+                let newRow        = logTableBody.insertRow();
+                let questionCell  = newRow.insertCell(0);
+                let gestureCell   = newRow.insertCell(1);
+                let timestampCell = newRow.insertCell(2);
+            
+                // Set the table cell values
+                questionCell.textContent  = question;
+                gestureCell.textContent   = gesture;
+                timestampCell.textContent = timestamp;
+    
+                loggedQuestions.push(question);
+    
+            }
+        });
+
+    });
+
 });
